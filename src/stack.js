0ba2a8bcd538170dd785d7476e1bf412c93b530e
SashaRPG <uasasharpg@gmail.com>
2022-04-30 18:23:02 +0300

    feat: stack DS was solved

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _data = [];
+  push(element) {
+    return this._data.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._data.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._data[this._data.length-1];
   }
 }
 
